ff657e615a9a2cbb96074592b6203e5a362f229d
NedcloarBR <nedcloar1@hotmail.com>
2025-03-19 16:31:36 -0300

    feat(backend): add openapi documentation

---
+++ apps/backend/src/lib/swagger.ts
@@ -0,0 +1,23 @@
+import { Logger } from "@nestjs/common";
+import type { NestFastifyApplication } from "@nestjs/platform-fastify";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+
+export function setupSwagger(app: NestFastifyApplication, globalPrefix: string, port: number): void {
+	const logger = new Logger("Documentation");
+	const documentConfig = new DocumentBuilder()
+		.setTitle("NestJS Initializr")
+		.setDescription("API Documentation for the NestJS Initializr")
+		.setVersion("v1.0.0")
+		.build();
+
+	const documentFactory = () => SwaggerModule.createDocument(app, documentConfig);
+
+	SwaggerModule.setup(`${globalPrefix}/docs`, app, documentFactory, {
+		jsonDocumentUrl: `${globalPrefix}/docs/json`,
+		yamlDocumentUrl: `${globalPrefix}/docs/yaml`,
+	});
+
+	logger.verbose(`Swagger is available at: http://localhost:${port}/${globalPrefix}/docs`);
+	logger.verbose(`JSON is available at: http://localhost:${port}/${globalPrefix}/docs/json`);
+	logger.verbose(`YAML is available at: http://localhost:${port}/${globalPrefix}/docs/yaml`);
+}
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -4,6 +4,7 @@ import { FastifyAdapter, type NestFastifyApplication } from "@nestjs/platform-fa
 import { AppModule } from "./app/app.module";
 import { Services } from "./app/constants/services";
 import type { ConfigService } from "./app/modules/config/config.service";
+import { setupSwagger } from "./lib";
 
 async function bootstrap() {
 	const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
@@ -11,8 +12,11 @@ async function bootstrap() {
 	const globalPrefix = configService.get("BACKEND_GLOBAL_PREFIX");
 	app.setGlobalPrefix(globalPrefix);
 	const port = configService.get("BACKEND_PORT");
+
+	setupSwagger(app, globalPrefix, port);
+
 	await app.listen(port, "0.0.0.0");
-	Logger.log(`Application is running on: http://localhost:${port}/${globalPrefix}`);
+	Logger.log(`Application is running on: http://localhost:${port}/${globalPrefix}`, "Bootstrap");
 }
 
 bootstrap();
