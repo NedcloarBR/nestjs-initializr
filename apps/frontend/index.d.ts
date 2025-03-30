2e109f26f74b75d2a0ffbf28a78c58d1b61584cf
NedcloarBR <nedcloar1@hotmail.com>
2025-03-30 11:06:00 -0300

    chore: remove eslint rules comments

--- apps/frontend/index.d.ts
+++ apps/frontend/index.d.ts
@@ -1,6 +1,8 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-declare module '*.svg' {
-  const content: any;
-  export const ReactComponent: any;
-  export default content;
+declare module "*.svg" {
+	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+	const content: any;
+	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+	export const ReactComponent: any;
+	// biome-ignore lint/style/noDefaultExport: <explanation>
+	export default content;
 }
--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -1,6 +1,4 @@
 //@ts-check
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { composePlugins, withNx } = require("@nx/next");
 const createNextIntlPlugin = require("next-intl/plugin");
 const withNextIntl = createNextIntlPlugin("./src/lib/i18n/request.ts");
@@ -13,18 +11,18 @@ const nextConfig = {
 	nx: {
 		// Set this to true if you would like to use SVGR
 		// See: https://github.com/gregberge/svgr
-		svgr: false,
+		svgr: false
 	},
 	eslint: {
-		ignoreDuringBuilds: true,
+		ignoreDuringBuilds: true
 	},
-	trailingSlash: false,
+	trailingSlash: false
 };
 
 const plugins = [
 	// Add more Next.js plugins to this list if needed.
 	withNx,
-	withNextIntl,
+	withNextIntl
 ];
 
 module.exports = composePlugins(...plugins)(nextConfig);
