6754bdeb638885388483daff1318fd9c15e84cac
NedcloarBR <nedcloar1@hotmail.com>
2025-04-15 17:42:13 -0300

    feat: remove unused files

--- apps/backend/src/app/modules/generator/generators/base/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "../base.generator";
-// export * from "./module-updater.generator";
-// export * from "./module.generator";
--- apps/backend/src/app/modules/generator/generators/base/module.generator.ts
+++
@@ -1,7 +0,0 @@
-// import { BaseGenerator } from "../base.generator";
-
-// export function ModuleGenerator(id: string, moduleTemplates: { name: string; path: string; content: string }[]) {
-// 	for (const template of moduleTemplates) {
-// 		BaseGenerator(id, template, true);
-// 	}
-// }
