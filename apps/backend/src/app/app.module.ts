7cf6f687f5167f44ad2cd3de74091538b2fc4cb5
NedcloarBR <nedcloar1@hotmail.com>
2025-04-18 17:01:18 -0300

    feat(backend): add rate limit and delete generated files after 10s

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,8 +1,26 @@
 import { Module } from "@nestjs/common";
+import { APP_GUARD } from "@nestjs/core";
+import { ThrottlerGuard, ThrottlerModule } from "@nestjs/throttler";
 import { ConfigModule, GeneratorModule } from "./modules";
 @Module({
-	imports: [ConfigModule, GeneratorModule],
+	imports: [
+		ConfigModule,
+		ThrottlerModule.forRoot({
+			throttlers: [
+				{
+					ttl: 60_000,
+					limit: 10
+				}
+			]
+		}),
+		GeneratorModule
+	],
 	controllers: [],
-	providers: []
+	providers: [
+		{
+			provide: APP_GUARD,
+			useClass: ThrottlerGuard
+		}
+	]
 })
 export class AppModule {}
--- apps/backend/src/app/modules/generator/generator.service.ts
+++ apps/backend/src/app/modules/generator/generator.service.ts
@@ -7,135 +7,149 @@ import { expressPackages } from "../../constants/express-packages";
 import { fastifyPackages } from "../../constants/fastify-packages";
 import { MetadataDTO } from "./dtos/metadata.dto";
 import { BaseGenerator } from "./generators/base.generator";
+import { MainUpdaterService } from "./generators/main-updater.service";
 import { ModuleService } from "./generators/module.service";
 import { PackageJsonService } from "./generators/package-json.service";
 import {
-  appControllerTemplate,
-  appModuleTemplate,
-  appServiceTemplate,
-  mainExpressTemplate,
-  mainFastifyTemplate,
-  nestjsCli,
-  readmeTemplate
+	appControllerTemplate,
+	appModuleTemplate,
+	appServiceTemplate,
+	mainExpressTemplate,
+	mainFastifyTemplate,
+	nestjsCli,
+	readmeTemplate
 } from "./templates";
 import { modulesTemplates } from "./templates/modules.template";
 import { tsconfig, tsconfigBuild } from "./templates/tsconfig.template";
-import { MainUpdaterService } from "./generators/main-updater.service";
 
 @Injectable()
 export class GeneratorService extends BaseGenerator {
-  public constructor(
-    private readonly packageJsonGenerator: PackageJsonService,
-    private readonly moduleGenerator: ModuleService,
-    private readonly mainUpdater: MainUpdaterService
-  ) {
-    super();
-  }
+	public constructor(
+		private readonly packageJsonGenerator: PackageJsonService,
+		private readonly moduleGenerator: ModuleService,
+		private readonly mainUpdater: MainUpdaterService
+	) {
+		super();
+	}
 
-  public async generate(metadata: MetadataDTO): Promise<fs.ReadStream> {
-    const id = Date.now().toString();
-    const packageJson = await this.packageJsonGenerator.generate(metadata.packageJson, id);
-    const rootDirFiles = [packageJson];
+	public async generate(metadata: MetadataDTO): Promise<fs.ReadStream> {
+		const id = Date.now().toString();
+		const packageJson = await this.packageJsonGenerator.generate(metadata.packageJson, id);
+		const rootDirFiles = [packageJson];
 
-    const packageManagers = {
-      npm: "package-lock.json",
-      yarn: "yarn.lock",
-      pnpm: "pnpm-lock.yaml"
-    }
-    const lockFile = this.createFile(id, {
-      name: packageManagers[metadata.packageManager],
-      path: "",
-      content: ""
-    });
-    rootDirFiles.push(lockFile);
+		const packageManagers = {
+			npm: "package-lock.json",
+			yarn: "yarn.lock",
+			pnpm: "pnpm-lock.yaml"
+		};
+		const lockFile = this.createFile(id, {
+			name: packageManagers[metadata.packageManager],
+			path: "",
+			content: ""
+		});
+		rootDirFiles.push(lockFile);
 
-    switch (metadata.mainType) {
-      case "express":
-        this.createFile(id, mainExpressTemplate);
-        for (const packageMeta of expressPackages) {
-          await this.packageJsonGenerator.addPackage(id, packageMeta.name, packageMeta.version, packageMeta.dev);
-        }
-        break;
-      case "fastify":
-        this.createFile(id, mainFastifyTemplate);
-        for (const packageMeta of fastifyPackages) {
-          await this.packageJsonGenerator.addPackage(id, packageMeta.name, packageMeta.version, packageMeta.dev);
-        }
-        break;
-    }
-    for (const packageMeta of commonPackages) {
-      await this.packageJsonGenerator.addPackage(id, packageMeta.name, packageMeta.version, packageMeta.dev);
-    }
+		switch (metadata.mainType) {
+			case "express":
+				this.createFile(id, mainExpressTemplate);
+				for (const packageMeta of expressPackages) {
+					await this.packageJsonGenerator.addPackage(id, packageMeta.name, packageMeta.version, packageMeta.dev);
+				}
+				break;
+			case "fastify":
+				this.createFile(id, mainFastifyTemplate);
+				for (const packageMeta of fastifyPackages) {
+					await this.packageJsonGenerator.addPackage(id, packageMeta.name, packageMeta.version, packageMeta.dev);
+				}
+				break;
+		}
+		for (const packageMeta of commonPackages) {
+			await this.packageJsonGenerator.addPackage(id, packageMeta.name, packageMeta.version, packageMeta.dev);
+		}
 
-    this.createFile(id, appModuleTemplate);
-    this.createFile(id, appControllerTemplate);
-    this.createFile(id, appServiceTemplate);
+		this.createFile(id, appModuleTemplate);
+		this.createFile(id, appControllerTemplate);
+		this.createFile(id, appServiceTemplate);
 
-    rootDirFiles.push(...[
-      this.createFile(id, tsconfig),
-      this.createFile(id, tsconfigBuild),
-      this.createFile(id, nestjsCli),
-      this.createFile(id, readmeTemplate)
-    ])
+		rootDirFiles.push(
+			...[
+				this.createFile(id, tsconfig),
+				this.createFile(id, tsconfigBuild),
+				this.createFile(id, nestjsCli),
+				this.createFile(id, readmeTemplate)
+			]
+		);
 
-    if (metadata.modules.length > 0) {
-      this.createFile(id, { name: "index.ts", path: "src/modules", content: "" });
-    }
+		if (metadata.modules.length > 0) {
+			this.createFile(id, { name: "index.ts", path: "src/modules", content: "" });
+		}
 
-    for (const module of metadata.modules) {
-      const moduleFiles = modulesTemplates.find((m) => m.name === module);
-      const moduleRootFiles = await this.moduleGenerator.generate(id, moduleFiles.templates, {
-        export: moduleFiles.constants.exporter,
-        import: moduleFiles.constants.name,
-        importIn: "src/app.module.ts"
-      });
-      rootDirFiles.push(...moduleRootFiles);
-      if (moduleFiles.constants.serviceConstant) {
-        this.moduleGenerator.updateServiceConstants(id, moduleFiles.constants.serviceConstant);
-      }
-      if (moduleFiles.mainTemplates) {
-        const templates = moduleFiles.mainTemplates(metadata.mainType);
-        for (const template of templates) {
-          this.mainUpdater.update(id, template);
-        }
-      }
-    }
+		for (const module of metadata.modules) {
+			const moduleFiles = modulesTemplates.find((m) => m.name === module);
+			const moduleRootFiles = await this.moduleGenerator.generate(id, moduleFiles.templates, {
+				export: moduleFiles.constants.exporter,
+				import: moduleFiles.constants.name,
+				importIn: "src/app.module.ts"
+			});
+			rootDirFiles.push(...moduleRootFiles);
+			if (moduleFiles.constants.serviceConstant) {
+				this.moduleGenerator.updateServiceConstants(id, moduleFiles.constants.serviceConstant);
+			}
+			if (moduleFiles.mainTemplates) {
+				const templates = moduleFiles.mainTemplates(metadata.mainType);
+				for (const template of templates) {
+					this.mainUpdater.update(id, template);
+				}
+			}
+		}
 
-    return await this.generateZipFile(rootDirFiles, id);
-  }
+		const zipFile = await this.generateZipFile(rootDirFiles, id);
 
-  private async generateZipFile(
-    files: {
-      fileName: string;
-      stream: fs.ReadStream;
-    }[],
-    id: string
-  ): Promise<fs.ReadStream> {
-    const dirPath = path.join(__dirname, "__generated__", id);
-    const srcPath = path.join(dirPath, "src");
-    const zipPath = path.join(dirPath, "project.zip");
+		setTimeout(() => {
+			fs.rm(path.join(__dirname, "__generated__", id), { recursive: true }, (err) => {
+				if (err) {
+					console.error(`Error deleting directory: ${err}`);
+				} else {
+					console.log(`Directory deleted successfully: ${id}`);
+				}
+			});
+		}, 10_000);
 
-    this.mkdir(dirPath);
+		return zipFile;
+	}
 
-    const output = fs.createWriteStream(zipPath);
-    const archive = archiver("zip", {
-      zlib: { level: 9 }
-    });
+	private async generateZipFile(
+		files: {
+			fileName: string;
+			stream: fs.ReadStream;
+		}[],
+		id: string
+	): Promise<fs.ReadStream> {
+		const dirPath = path.join(__dirname, "__generated__", id);
+		const srcPath = path.join(dirPath, "src");
+		const zipPath = path.join(dirPath, "project.zip");
 
-    archive.pipe(output);
+		this.mkdir(dirPath);
 
-    for (const file of files) {
-      archive.append(file.stream, { name: file.fileName });
-    }
+		const output = fs.createWriteStream(zipPath);
+		const archive = archiver("zip", {
+			zlib: { level: 9 }
+		});
 
-    archive.directory(srcPath, "src");
+		archive.pipe(output);
 
-    await new Promise((resolve, reject) => {
-      output.on("close", resolve);
-      output.on("error", reject);
-      archive.finalize();
-    });
+		for (const file of files) {
+			archive.append(file.stream, { name: file.fileName });
+		}
 
-    return fs.createReadStream(zipPath);
-  }
+		archive.directory(srcPath, "src");
+
+		await new Promise((resolve, reject) => {
+			output.on("close", resolve);
+			output.on("error", reject);
+			archive.finalize();
+		});
+
+		return fs.createReadStream(zipPath);
+	}
 }
