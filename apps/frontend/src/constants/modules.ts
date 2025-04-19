4068c24908c1bd3ca0e1df9dcc1fc061fa58bbb8
NedcloarBR <nedcloar1@hotmail.com>
2025-04-19 20:15:29 -0300

    feat(frontend): add swagger module option

--- apps/frontend/src/constants/modules.ts
+++ apps/frontend/src/constants/modules.ts
@@ -3,5 +3,6 @@ export const modules = (t: (args: string) => string) => [
 		title: "@nestjs/config",
 		name: "config",
 		description: t("Modules.config.description")
-	}
+	},
+	{ title: "Swagger", name: "swagger", description: t("Modules.swagger.description") }
 ];
